1bbfb3725f8c995ab3a430092448fd2dea1429ef
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-08 14:39:51 +0500

    Update Permutation Coefficient.py
    Optimised script by using FACTORIAL function

--- Permutation Coefficient.py
+++ Permutation Coefficient.py
@@ -1,8 +1,7 @@
-from math import prod
+from math import factorial
 
 def permutation_coefficient(x, y): # Permutation Coefficient
     
-    return prod(range(1, x + 1)) / prod(range(1, x - y + 1))
-
-result = permutation_coefficient(59, 6) # Test
-print(result)
+    return factorial(x) / factorial(x - y)
+    
+print(permutation_coefficient(5, 2)) # Test
