23e38bdb8e3389214440cf7691f465ff02b0e52f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-08 14:37:06 +0500

    Update Binomial Distribution.py
    Rewrote formulae by using factorial function from MATH package

--- Binomial Distribution.py
+++ Binomial Distribution.py
@@ -1,14 +1,7 @@
+from math import factorial
+
 def binomial_distribution(x, y, p): # Binomial Distribution
     
-    if y == 0: # When y = 0 => 0! = 1
-        
-        result = (prod(range(1, x + 1)) /
-                  (prod(range(1,1))*prod(range(1,x-y+1))))*p**y*(1-p)**(x-y)
-    else:
-        # For other cases
-        result = (prod(range(1, x + 1)) /
-                  (prod(range(1,y+1))*prod(range(1,x-y+1))))*p**y*(1-p)**(x-y)
+    return (factorial(x)/(factorial(y)*factorial(x-y)))*p**y*(1-p)**(x-y)
     
-    return result
-
 print(binomial_distribution(4, 0, 0.1)) # Test
