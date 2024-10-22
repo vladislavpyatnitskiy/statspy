6d7a1cc567b55b4275dce211e212204abff0084a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-22 12:24:08 +0500

    Update Binomial Distribution.py
    Added option to get probability for several events

--- Binomial Distribution.py
+++ Binomial Distribution.py
@@ -2,6 +2,9 @@ from math import factorial
 
 def binomial_distribution(x, y, p): # Binomial Distribution
     
-    return (factorial(x)/(factorial(y)*factorial(x-y)))*p**y*(1-p)**(x-y)
-    
-print(binomial_distribution(4, 0, 0.1)) # Test
+    if isinstance(y, int):
+        return math.comb(x, y) * p**y * (1 - p)**(x - y)
+    else:
+        return sum([math.comb(x, n) * p**n * (1 - p)**(x - n) for n in y])
+
+binomial_distribution(4, 0, 0.1) # test
