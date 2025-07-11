653b94c4a480654b1af53266dc5ad316218b4913
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-11 11:06:36 +0500

    Update Binomial Distribution.py
    Optimised script:
    
    • Replaced sublibrary
    • Reduced unnecessary ELSE statement

--- Binomial Distribution.py
+++ Binomial Distribution.py
@@ -1,10 +1,10 @@
-from math import factorial
+from math import comb
 
 def binomial_distribution(x, y, p): # Binomial Distribution
     
     if isinstance(y, int):
-        return math.comb(x, y) * p**y * (1 - p)**(x - y)
-    else:
-        return sum([math.comb(x, n) * p**n * (1 - p)**(x - n) for n in y])
+        return comb(x, y) * p**y * (1 - p)**(x - y)
+    
+    return sum([comb(x, n) * p**n * (1 - p)**(x - n) for n in y])
 
 binomial_distribution(4, 0, 0.1) # test
