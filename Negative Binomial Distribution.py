e692f24dac752855ce726d7e7b2dfeff7f78920d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-13 11:16:52 +0500

    Update Negative Binomial Distribution.py
    Optimised script by eliminating unnecessary ELSE statement

--- Negative Binomial Distribution.py
+++ Negative Binomial Distribution.py
@@ -2,9 +2,8 @@ from math import factorial
 
 def binomial_distribution_negative(x, y, p): # Negative Binomial Distribution
     if y == 0:
-        print("Use Geometric Distribution Please")
-    else:
-        r=(factorial(x-1)/(factorial(y-1)*factorial(x-y-1)))*p**y*(1-p)**(x-y)
-        return r
+        return("Use Geometric Distribution Please")
+    
+    return factorial(x-1)/(factorial(y-1)/factorial(x-y-1))*p**y*(1-p)**(x-y)
 
-print(binomial_distribution_negative(4, 1, 0.9)) # Test
+binomial_distribution_negative(4, 1, 0.9) # Test
